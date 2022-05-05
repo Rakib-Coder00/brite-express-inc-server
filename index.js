a45a2b66e3c2b718d099d8925cbe6f3afc5b06a2
Rakib-Dev <rhbappy456@gmail.com>
2022-05-05 11:39:40 +0600

    mongoDB connected

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const port = process.env.PORT || 5000
 
@@ -12,9 +13,42 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get('/', (req, res)=>{
-    res.send('Brite Express server running...✅')
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mwhxt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const serviceCollection = client.db('briteExpress').collection('services')
+
+
+        //Services API
+        app.get('/service', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   console.log('mongoDB connected');
+//   client.close();
+// });
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Brite Express server running...✔')
 })
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log('Listening to port', port);
 })
