acc174d7b07b9107abbbcd1d68248c849ea9c03b
Rakib-Dev <rhbappy456@gmail.com>
2022-05-07 16:37:00 +0600

    Delete items from the database

--- index.js
+++ index.js
@@ -50,6 +50,15 @@ async function run() {
             const result = await serviceCollection.insertOne(newItem)
             res.json(result)
         })
+
+        //Delete items from the database =>
+        app.delete('/service/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const result = await serviceCollection.deleteOne(query)
+            res.json(result)
+        })
+        
     }
     finally {
 
